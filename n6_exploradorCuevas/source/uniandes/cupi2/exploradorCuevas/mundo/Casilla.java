c531209cdee8c9efac50c0e90e90ad6b0220a732
js.prieto10 <js.prieto10@uniandes.edu.co>
2017-11-19 22:13:39 -0500

    cambios mundo casillas y leer

--- n6_exploradorCuevas/source/uniandes/cupi2/exploradorCuevas/mundo/Casilla.java
+++ n6_exploradorCuevas/source/uniandes/cupi2/exploradorCuevas/mundo/Casilla.java
@@ -1,41 +1,25 @@
 package uniandes.cupi2.exploradorCuevas.mundo;
 
 public class Casilla {
-	public final static int VACIA = 0;
-	public final static int OBSTACULO= 1;
-	public final static int LLAVE_ROJA =2;
-	public final static int LLAVE_AZUL =3;
-	public final static int LLAVE_AMARILLA =4;
-	public final static int PUERTA_ROJA =5;
-	public final static int PUERTA_AZUL =6;
-	public final static int PUERTA_AMARILLA = 7;
-	public final static int SALIDA =8;
-	public final static int CHIP =9;
-	public final static int PREMIO = 10;
-	public final static int JUGADOR = 11;
+	public final static char NADA = 'N';
+	public final static char JUGADOR = 'J';
+	public final static char CERCANA = 'C';
+	public final static char OBSTACULO = 'O';
+	public final static char TESORO = 'T';
+	public final static char BOMBA = 'B';
+	
 	private int estado;
 	private int columna;
 	private int fila;
-	private int visitas;
 
 	public Casilla (int pEstado, int pFila, int pColumna){
 		estado = pEstado;
 		columna = pColumna;
 		fila = pFila;
-		visitas = 0;
 	}
 	public int darEstado()
 	{
 		return estado; 
-	}
-	public void agregarVisitas(){
-		visitas++;		
-	}
-	public int darVisitas(){
-		return visitas;
-	}
-	public void reiniciar(){
-		visitas = 0;
 	}
 	public int darFila() {
 		return fila;
